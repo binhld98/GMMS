53107b71b2e260ed78b408b943be5ccb839c813f
BinhLD <8991nofear@gmail.com>
2023-09-08 16:30:41 +0700

    add left panel

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,14 +5,17 @@ import { IconsProviderModule } from './icons-provider.module';
 import { PagesComponent } from './pages.component';
 import { PagesRoutingModule } from './pages-routing.module';
 
+import { GroupComponent } from './group/group.component';
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzListModule } from 'ng-zorro-antd/list';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzMessageModule } from 'ng-zorro-antd/message';
 import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { GroupComponent } from './group/group.component';
 
 @NgModule({
   declarations: [PagesComponent, WelcomeComponent, GroupComponent],
@@ -23,8 +26,11 @@ import { GroupComponent } from './group/group.component';
 
     IconsProviderModule,
     NzAvatarModule,
+    NzCardModule,
     NzDropDownModule,
+    NzGridModule,
     NzLayoutModule,
+    NzListModule,
     NzMenuModule,
     NzMessageModule,
     NzPageHeaderModule,
