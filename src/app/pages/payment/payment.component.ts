f117e1f7d89dd3252478012e9bac7344ce9f6835
BinhLD <8991nofear@gmail.com>
2023-09-30 14:28:20 +0700

    payment serach form

--- src/app/pages/payment/payment.component.ts
+++ src/app/pages/payment/payment.component.ts
@@ -1,14 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+import { GroupMasterDto } from 'src/app/@core/dtos/group.dto';
 
 @Component({
   selector: 'gmm-payment',
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.css'],
 })
-export class PaymentComponent {
+export class PaymentComponent implements OnInit {
   isVisibleUpsert = false;
+  form!: FormGroup;
+  isLoadingPayments = false;
+  groups: GroupMasterDto[] = [];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    const now = new Date();
+    const startOfCurrentMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+    const endOfCurrentMonth = new Date(
+      now.getFullYear(),
+      now.getMonth() + 1,
+      0
+    );
+
+    this.form = this.fb.group({
+      groupId: ['-1', [Validators.required]],
+      fromToType: ['payment_at', [Validators.required]],
+      fromDate: [startOfCurrentMonth, [Validators.required]],
+      toDate: [endOfCurrentMonth, [Validators.required]],
+    });
+  }
 
   onAddPayment() {
     this.isVisibleUpsert = true;
+  }
+
+  onSearchPayments() {
+    if (this.form.invalid) {
+      Object.values(this.form.controls).forEach((c) => {
+        if (c instanceof FormControl) {
+          c.markAsDirty();
+          c.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+      return;
+    }
+    // this.isLoadingPayments = true;
   }
 }
