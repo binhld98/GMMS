631a1346ad5a8f1783792fb445c6af1bd46dbe8e
BinhLD <8991nofear@gmail.com>
2023-10-03 21:09:44 +0700

    static load icons + off animation + vi locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
+
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideStorage, getStorage } from '@angular/fire/storage';
-import { NZ_I18N, vi_VN } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import vi from '@angular/common/locales/vi';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-registerLocaleData(vi);
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { NZ_I18N, NZ_DATE_LOCALE, vi_VN } from 'ng-zorro-antd/i18n';
+import { vi } from 'date-fns/locale';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NoopAnimationsModule,
+
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
   ],
-  providers: [{ provide: NZ_I18N, useValue: vi_VN }],
+  providers: [
+    { provide: NZ_I18N, useValue: vi_VN },
+    { provide: NZ_DATE_LOCALE, useValue: vi },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
